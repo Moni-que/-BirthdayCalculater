5d6d9c11e1e68df96cf5cb42ee00cf931e1f7cc7
moni-que <monicahjustus@gmail.com>
2022-03-07 05:21:58 +0300

    add constant akan names

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-const days=["Sunday","Monday","Tuesday","wednesday","Thursday","Friday","Saturday"]
+const days=["Sunday","Monday","Tuesday","wednesday","Thursday","Friday","Saturday"];
+const femaleAkanNames={
+    Sunday:"Akosua",
+    Monday:"Adwoa",
+    Tuesday:"Abenaa",
+    Wednesday:"Akua",
+    Thursday:"Yaa",
+    Friday:"Afua",
+    Saturday:"Ama"
+}
+const maleAkanNames={
+    Sunday:"Kwasi",
+    Monday:"Kwadwo",
+    Tuesday:"Kwabena",
+    Wednesday:"Kwaku",
+    Thursday:"Yaw",
+    Friday:"Kofi",
+    Saturday:"Kwame"
+};
