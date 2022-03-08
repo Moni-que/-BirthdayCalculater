4e1cb0d8735771fbd7a19d0aa32878d112e762a1
moni-que <monicahjustus@gmail.com>
2022-03-08 09:00:56 +0300

    add variables for function

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-const days=["Sunday","Monday","Tuesday","wednesday","Thursday","Friday","Saturday"];
-const femaleAkanNames={
-    Sunday:"Akosua",
-    Monday:"Adwoa",
-    Tuesday:"Abenaa",
-    Wednesday:"Akua",
-    Thursday:"Yaa",
-    Friday:"Afua",
-    Saturday:"Ama"
+// function myDetails() {
+//     let days = ["Sunday","Monday","Tuesday","wednesday","Thursday","Friday","Saturday"];
+//     let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+//     let femaleAkanNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+//     let maleAkanNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+
+// }
+
+// writing a function with a variable
+function time() {
+    let name = document.getElementById("name").value;
+    let day = document.getElementById("day").value;
+    let month = document.getElementById("month").value;
+    let year = document.getElementById("year").value;
+    let gender = document.getElementById("gender").value
+    let endMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let today = new Date ();
+    let realDays = today.getDate ();
+    let realMonths = today.getMonth ();
+    let realYears = today.getFullYear ();
 }
-const maleAkanNames={
-    Sunday:"Kwasi",
-    Monday:"Kwadwo",
-    Tuesday:"Kwabena",
-    Wednesday:"Kwaku",
-    Thursday:"Yaw",
-    Friday:"Kofi",
-    Saturday:"Kwame"
-};
+// writing if statements to display answers
